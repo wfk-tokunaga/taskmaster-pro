8fa1eed4e2b6f1250b915b53b57da1badd89c077
Will Tokunaga <wktokunaga@gmail.com>
2022-04-04 09:02:36 -0700

    added drag and drop function

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,6 @@ var loadTasks = function () {
   });
 };
 
-
 var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
@@ -171,6 +170,67 @@ $("#remove-tasks").on("click", function () {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+// .sortable() turns every elem with class ".card .list-group into a sortable list
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  // activate: event => console.log("activate", this),
+  // deactivate: event => console.log("deactivate", this),
+  // over: event => console.log("over", event.target),
+  // out: event => console.log("out", event.target),
+  update: function (event) {
+    var tempArr = [];
+
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      tempArr.push({
+        text,
+        date,
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // Okay, so from the this object, we get it's list name with 
+    // the .attr("id") method
+    // this returns something like "list-toDo"
+    // We then get rid of the "list-" part with .replace()
+    // Now we just have the toDo, which we use to index into the right property
+    // of our tasks object
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
+  }
 });
 
 // load tasks for the first time
